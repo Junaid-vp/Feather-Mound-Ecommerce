c01fdb2751c0e8c0fa79c8064771f42e631c48d8
Muhammad Junaid <jasaanu123@gmail.com>
2026-03-26 19:43:43 +0530

    updated changes

--- Front_End/vite.config.js
+++ Front_End/vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1200,
+  },
   plugins: [
     react(),
     tailwindcss(),
